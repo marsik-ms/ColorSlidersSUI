02904f21750a7f8b45b4856a617cfeeb2d962019
marsik-ms <79077490+marsik-ms@users.noreply.github.com>
2023-06-13 19:59:02 +0300

    Initial Commit

---
+++ ColorSliders/ColorSlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSlidersApp.swift
+//  ColorSliders
+//
+//  Created by Stanislav Perepechin on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorSlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
